58e77b7a2863f75aa6dbc597f481980b69fee9a2
DillonHHH <dillonisaiahhines02@gmail.com>
2022-02-20 17:43:43 -0600

    splash screen should now be resized and centered

--- Main.py
+++ Main.py
@@ -2,14 +2,27 @@ import pygame
 
 pygame.init
 
-screen = pygame.display.set_mode([800, 800])
+screen = pygame.display.set_mode([720, 480])
+
+
 splashScreen = pygame.image.load('splashScreen.png')
-splashScreen = pygame.transform.scale(splashScreen, pygame.display.get_surface().get_size())#Scales the splash screen to the size of the window
+width, height = pygame.display.get_surface().get_size()
+splashScreenPosition = (0,0) #splashscreen is positioned at the top left corner of the screen
+
+#following lines just prevent the splash screen from stretching if the window is rectangular
+if(width > height):
+    splashScreenPosition = ((width - height) / 2,0) #centers the image horizontally
+    width = height
+elif(height > width):
+    splashScreenPosition = ((height - width) / 2,0) #centers the image vertically
+    height = width
+
+splashScreen = pygame.transform.scale(splashScreen, (width,height))#Scales the splash screen to the size of the window
 
 
 while True:
     screen.fill((75,75,75)) #screen filled with gray
-    screen.blit(splashScreen, (0,0)) #splashscreen is drawn at the top left corner of the screen
+    screen.blit(splashScreen, splashScreenPosition) 
     pygame.display.flip()         
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
