0766948faa135b7d8053fb1c90f0f2d3252a4b44
DillonHHH <dillonisaiahhines02@gmail.com>
2022-02-20 17:31:29 -0600

    program base

--- Main.py
+++ Main.py
@@ -1,5 +1,18 @@
-#test
+import pygame
 
-print("testing")
-print("testing")
-#test test
+pygame.init
+
+screen = pygame.display.set_mode([800, 800])
+splashScreen = pygame.image.load('splashScreen.png')
+splashScreen = pygame.transform.scale(splashScreen, pygame.display.get_surface().get_size())#Scales the splash screen to the size of the window
+
+
+while True:
+    screen.fill((75,75,75)) #screen filled with gray
+    screen.blit(splashScreen, (0,0)) #splashscreen is drawn at the top left corner of the screen
+    pygame.display.flip()         
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.display.quit(), sys.exit()
+
+
