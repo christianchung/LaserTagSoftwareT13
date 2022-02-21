3ed8010d0d481ce8c80e7527badd6ad1bb3e4fd2
DillonHHH <dillonisaiahhines02@gmail.com>
2022-02-20 19:03:47 -0600

    Added the base for the playerlist

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 import pygame
+import time #for the sleep function
 
 pygame.init
 
-screen = pygame.display.set_mode([720, 480])
+screen = pygame.display.set_mode([800, 800])
 
 
-splashScreen = pygame.image.load('splashScreen.png')
+splashScreen = pygame.image.load("splashScreen.png")
 width, height = pygame.display.get_surface().get_size()
 splashScreenPosition = (0,0) #splashscreen is positioned at the top left corner of the screen
 
@@ -19,13 +20,38 @@ elif(height > width):
 
 splashScreen = pygame.transform.scale(splashScreen, (width,height))#Scales the splash screen to the size of the window
 
-
-while True:
+#splash screen
+splashScreenTimer = 0 
+while (splashScreenTimer < 5 * 1): #splash screen is up for 1 second
     screen.fill((75,75,75)) #screen filled with gray
     screen.blit(splashScreen, splashScreenPosition) 
     pygame.display.flip()         
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit(), sys.exit()
+    splashScreenTimer += 1
+    time.sleep(.2) #only sleep for .2 seconds so that the program doesn't freeze from not responding to events
 
+#everything after the splash screen
+while True:
+    screen.fill((0,200,0)) #fill screen with red
+    screen.fill((200,0,0), (0, 0, screen.get_width() / 2, screen.get_height())) #fill right side of screen with screen
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.display.quit(), sys.exit()
+
+    #draws boxes
+    for x in range(20):
+        #small boxes
+        screen.fill((255,255,255), (30, x * 37 + 50, 120, 34))
+        screen.fill((255,255,255), (430, x * 37 + 50, 120, 34))
+
+        #big boxes
+        screen.fill((255,255,255), (155, x * 37 + 50, 240, 34))
+        screen.fill((255,255,255), (555, x * 37 + 50, 240, 34))
+
+
+
+
+    pygame.display.flip() #keep at end of while loop
 
