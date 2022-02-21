8bff5036609711d5f46acddb2ccf14e6e66e1352
Winphillips <98627220+Winphillips@users.noreply.github.com>
2022-02-21 00:17:39 -0600

    Text and proto clickable box with quit action+new logo

--- Main.py
+++ Main.py
@@ -1,57 +1,100 @@
+import sys
 import pygame
-import time #for the sleep function
+import tkinter
+import time # for the sleep function
 
-pygame.init
+pygame.init()
 
 screen = pygame.display.set_mode([800, 800])
 
-
-splashScreen = pygame.image.load("splashScreen.png")
+#=====================================================================
+#   Splash Screen
+#=====================================================================
+splashScreen = pygame.image.load("splashScreen2.png")
 width, height = pygame.display.get_surface().get_size()
-splashScreenPosition = (0,0) #splashscreen is positioned at the top left corner of the screen
+splashScreenPosition = (0,0) # splashscreen is positioned at the top left corner of the screen
 
-#following lines just prevent the splash screen from stretching if the window is rectangular
+# following lines just prevent the splash screen from stretching if the window is rectangular
 if(width > height):
-    splashScreenPosition = ((width - height) / 2,0) #centers the image horizontally
+    splashScreenPosition = ((width - height) / 2,0) # centers the image horizontally
     width = height
 elif(height > width):
-    splashScreenPosition = ((height - width) / 2,0) #centers the image vertically
+    splashScreenPosition = ((height - width) / 2,0) # centers the image vertically
     height = width
 
 splashScreen = pygame.transform.scale(splashScreen, (width,height))#Scales the splash screen to the size of the window
 
-#splash screen
 splashScreenTimer = 0 
-while (splashScreenTimer < 5 * 1): #splash screen is up for 1 second
-    screen.fill((75,75,75)) #screen filled with gray
+while (splashScreenTimer < 5 * 1): # splash screen is up for 1 second
+    screen.fill((5,225,255)) # screen filled with cyan
     screen.blit(splashScreen, splashScreenPosition) 
     pygame.display.flip()         
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit(), sys.exit()
     splashScreenTimer += 1
-    time.sleep(.2) #only sleep for .2 seconds so that the program doesn't freeze from not responding to events
+    time.sleep(.2) # only sleep for .2 seconds so that the program doesn't freeze from not responding to events
 
-#everything after the splash screen
+#=====================================================================
+#   Player Entry Screen
+#=====================================================================
+# light shade of the button
+checkBoxColor = (115,115,115)
+checkBoxColorHover = (75,75,75)
+# print(pygame.font.get_fonts())        # gets all fonts on system
+# initialize font to (defualt pygame font, fontSize)
+font = pygame.font.Font(None, 30)
+
 while True:
-    screen.fill((0,200,0)) #fill screen with red
-    screen.fill((200,0,0), (0, 0, screen.get_width() / 2, screen.get_height())) #fill right side of screen with screen
+    screen.fill((0,195,0)) # fill screen with GREEN
+    screen.fill((195,0,0), (0, 0, screen.get_width() / 2, screen.get_height())) # fill right side of screen with RED
+    screen.fill((115,115,115), (0, 720, screen.get_width(), screen.get_height()/10)) # fill Bottom bar for Functions with GRAY
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit(), sys.exit()
 
-    #draws boxes
+        # check for mouse click
+        if event.type == pygame.MOUSEBUTTONDOWN:
+              
+            # PROTOTYPE FOR CHECK BOXES - need to add color change or 'X' on click, as well as update a bool for each ID
+            # QUIT on mouse click
+            if 770 <= mouse[0] <= 770+28 and -2 <= mouse[1] <= 26:
+                pygame.quit()
+
+    mouse = pygame.mouse.get_pos() # store mouse position
+
+    # change box color on hover
+    if 770 <= mouse[0] <= 770+28 and -2 <= mouse[1] <= 26:
+        pygame.draw.rect(screen,checkBoxColor,[770,2,28,28])
+          
+    else:
+        pygame.draw.rect(screen,checkBoxColorHover,[770,2,28,28])
+
+    # Team texts
+    # add EDIT CURRENT GAME text
+    text = font.render("Edit Current Game", 1, (5,5,5)) # Black text color
+    screen.blit(text, (320, 5)) # position text on screen
+
+    # add RED TEAM text
+    text = font.render("Red Team", 1, (5,225,255)) # Cyan text color
+    screen.blit(text, (150, 25))
+
+    # add GREEN TEAM text
+    text = font.render("Green Team", 1, (5,225,255))
+    screen.blit(text, (550, 25))
+
+
+    # draws boxes
     for x in range(20):
-        #small boxes
-        screen.fill((255,255,255), (30, x * 37 + 50, 120, 34))
-        screen.fill((255,255,255), (430, x * 37 + 50, 120, 34))
+        
+        # small boxes
+        screen.fill((255,255,255), (30, x * 33 + 50, 120, 30))
+        screen.fill((255,255,255), (430, x * 33 + 50, 120, 30))
 
-        #big boxes
-        screen.fill((255,255,255), (155, x * 37 + 50, 240, 34))
-        screen.fill((255,255,255), (555, x * 37 + 50, 240, 34))
+        # big boxes
+        screen.fill((255,255,255), (155, x * 33 + 50, 240, 30))
+        screen.fill((255,255,255), (555, x * 33 + 50, 240, 30))
 
 
-
-
-    pygame.display.flip() #keep at end of while loop
-
+    pygame.display.flip() # keep at end of while loop
